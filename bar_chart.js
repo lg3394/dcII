68e2968a9a81f55550f29c2bcc94f1485fa75351
Lucia de la Torre <lg3394@columbia.edu>
2025-08-06 11:22:01 -0400

    Create bar_chart.js

---
+++ bar_chart.js
@@ -0,0 +1,8 @@
+// bar_chart.js
+d3.csv('data/energy_consumption_per_task.csv').then(data => {
+  // Set SVG dimensions, scales, and axes
+
+  // Map data to bars
+  // On hover, show tooltip with task and average Wh
+  // Add axis labels
+});
